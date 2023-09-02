f4ee8fc0bf1f5c6f033c6be13321690f50b08832
IMWoo94 <leemeawoo94@gmail.com>
2023-09-02 17:42:43 +0900

    2023.09.02 backjoon chap03 question10

--- src/main/java/chap03/question09/Main.java
+++ src/main/java/chap03/question09/Main.java
@@ -1,8 +1,8 @@
-package chap03.question07;
+package chap03.question09;
 
 /*
-	https://www.acmicpc.net/problem/11022
-	A+B -8
+	https://www.acmicpc.net/problem/10952
+	A+B -5
  */
 
 import java.io.BufferedReader;
@@ -15,15 +15,17 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int size = Integer.parseInt(br.readLine());
 
 		try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));) {
-			for (int i = 0; i < size; i++) {
+			while (true) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 				int A = Integer.parseInt(st.nextToken());
 				int B = Integer.parseInt(st.nextToken());
 				int result = A + B;
-				bw.write("Case #" + (i + 1) + ": " + A + " + " + B + " = " + result + "\n");
+				if (result == 0) {
+					break;
+				}
+				bw.write(result + "\n");
 			}
 			bw.flush();
 		} catch (IOException e) {
