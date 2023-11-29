3528e52f5a9b9411c71f73bcbb939f95408d00df
IMWoo94 <leemeawoo94@gmail.com>
2023-11-29 16:11:38 +0900

    template write 2023.11.29

---
+++ src/main/java/template.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class template {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			 BufferedWriter bw = new BufferedWriter(new PrintWriter(System.out))
+		) {
+
+		} catch (IOException e) {
+
+		}
+
+	}
+}
