9b2ffa55e08e3efbfc9d249691797d3227b436d4
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-01-11 16:52:13 +0900

    [level Lv.0] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        // StringBuilder str = new StringBuilder();
+        // for(int i = 0; i < k; i++){
+        //     str.append(my_string);
+        // }
+    
+        return my_string.repeat(k);
+    }
+}
