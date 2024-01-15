d941fa323437e30d989d80f4721e4c522a7e18ca
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-01-15 21:07:45 +0900

    [level Lv.2] Title: 의상, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.2/42578. 의상/의상.py
+++ 프로그래머스/Lv.2/42578. 의상/의상.py
@@ -1,10 +1,14 @@
+from collections import Counter
+
 def solution(clothes):
     answer = 1
-    kind = {}
-    for clothe in clothes:
-        kind[clothe[1]] = kind.get(clothe[1], 0)+ 1
+#     kind = {}
+#     for clothe in clothes:
+#         kind[clothe[1]] = kind.get(clothe[1], 0)+ 1
+    kind = dict(Counter([kind for name, kind in clothes]))
         
     for num in kind.values():
         answer *= num + 1
         
     return answer -1 
+    
