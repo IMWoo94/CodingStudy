0417147f1fa0afce15819f78fc6588bfea2f48a4
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-01-16 17:07:03 +0900

    [level Lv.0] Title: 홀수 vs 짝수, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int a = 0;
+        int b = 0;
+        
+        for(int i = 0; i < num_list.length; i++){
+            if(i % 2 == 0){
+                a += num_list[i];
+            }else{
+                b += num_list[i];
+            }
+            
+        }
+        
+        answer = Math.max(a,b);
+        return answer;
+    }
+}
