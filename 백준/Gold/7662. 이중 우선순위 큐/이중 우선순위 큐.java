dfb2258c3113222e40185f8050d80a348ef3f370
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-02-03 19:53:23 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2744 ms, Memory: 445916 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -2,11 +2,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
 	public static void main(String[] args) {
@@ -17,40 +14,29 @@ public class Main {
 			int T = Integer.parseInt(br.readLine());
 			while (T > 0) {
 				int K = Integer.parseInt(br.readLine());
-				PriorityQueue<Integer> min = new PriorityQueue<>();
-				PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
-				Map<Integer, Integer> index = new HashMap<>();
+				TreeMap<Integer, Integer> q = new TreeMap<>();
 				for (int i = 0; i < K; i++) {
 					StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 					String op = st.nextToken();
 					int num = Integer.parseInt(st.nextToken());
-
 					if (op.equals("I")) {
-						min.offer(num);
-						max.offer(num);
-						index.put(num, index.getOrDefault(num, 0) + 1);
+						q.put(num, q.getOrDefault(num, 0) + 1);
 					} else {
-						if (index.isEmpty())
+						if (q.isEmpty())
 							continue;
 
-						if (num == -1) {
-							// 최솟값 제거
-							removeMap(min, index);
-						} else {
-							// 최댓값 제거
-							removeMap(max, index);
-						}
+						int key = num == 1 ? q.lastKey() : q.firstKey();
+						if (q.put(key, q.get(key) - 1) == 1)
+							q.remove(key);
 					}
 				}
-				if (index.size() == 0)
+
+				if (q.isEmpty()) {
 					bw.write("EMPTY");
-				else {
-					int n = removeMap(max, index);
-					int m = !index.isEmpty() ? removeMap(min, index) : n;
-					bw.write(n + " " + m);
+				} else {
+					bw.write(q.lastKey() + " " + q.firstKey());
 				}
 				bw.newLine();
-				T--;
 			}
 			bw.flush();
 		} catch (Exception e) {
@@ -58,21 +44,4 @@ public class Main {
 		}
 
 	}
-
-	static int removeMap(PriorityQueue<Integer> que, Map<Integer, Integer> map) {
-		int num;
-		while (true) {
-			num = que.poll();
-			int cnt = map.getOrDefault(num, 0);
-			if (cnt == 0)
-				continue;
-			if (cnt == 1)
-				map.remove(num);
-			else
-				map.put(num, cnt - 1);
-			break;
-		}
-		return num;
-	}
-
 }
