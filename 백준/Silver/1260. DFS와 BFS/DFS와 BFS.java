020f7511a0028c1e518308916fe0e4b90ed4df18
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-02-06 18:59:51 +0900

    [Silver II] Title: DFS와 BFS, Time: 224 ms, Memory: 18936 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,7 +2,10 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -21,14 +24,27 @@ public class Main {
 			int start = Integer.parseInt(st.nextToken());
 
 			// 인접 행렬 생성
-			int[][] vertexes = new int[vertex + 1][vertex + 1];
+			// int[][] vertexes = new int[vertex + 1][vertex + 1];
+			// for (int i = 0; i < edge; i++) {
+			// 	StringTokenizer temp = new StringTokenizer(br.readLine(), " ");
+			// 	int begin = Integer.parseInt(temp.nextToken());
+			// 	int end = Integer.parseInt(temp.nextToken());
+			// 	vertexes[begin][end] = 1;
+			// 	vertexes[end][begin] = 1;
+			// }
+			// 인접 리스트 생성
+			ArrayList<Integer>[] vertexes = new ArrayList[vertex + 1];
+			for (int i = 0; i < vertex + 1; i++) {
+				vertexes[i] = new ArrayList<>();
+			}
 			for (int i = 0; i < edge; i++) {
 				StringTokenizer temp = new StringTokenizer(br.readLine(), " ");
 				int begin = Integer.parseInt(temp.nextToken());
 				int end = Integer.parseInt(temp.nextToken());
-				vertexes[begin][end] = 1;
-				vertexes[end][begin] = 1;
+				vertexes[begin].add(end);
+				vertexes[end].add(begin);
 			}
+
 			boolean[] visited = new boolean[vertex + 1];
 			dfs(start, visited, vertexes);
 
@@ -45,20 +61,20 @@ public class Main {
 
 	}
 
-	public static void dfs(int now, boolean[] visited, int[][] vertexes) {
+	public static void dfs(int now, boolean[] visited, ArrayList<Integer>[] vertexes) {
 		visited[now] = true;
 		dfsAns.append(now).append(" ");
-		for (int i = 1; i < vertexes.length; i++) {
-			if (vertexes[now][i] == 0) {
-				continue;
-			}
-			if (!visited[i]) {
-				dfs(i, visited, vertexes);
+		ArrayList<Integer> temp = vertexes[now];
+		Collections.sort(temp);
+		for (int i = 0; i < temp.size(); i++) {
+			int index = temp.get(i);
+			if (!visited[index]) {
+				dfs(index, visited, vertexes);
 			}
 		}
 	}
 
-	public static void bfs(int now, boolean[] visited, int[][] vertexes) {
+	public static void bfs(int now, boolean[] visited, ArrayList<Integer>[] vertexes) {
 		Queue<Integer> q = new LinkedList<>();
 		q.offer(now);
 		visited[now] = true;
@@ -66,15 +82,18 @@ public class Main {
 		while (!q.isEmpty()) {
 			int start = q.poll();
 			bfsAns.append(start).append(" ");
-			for (int i = 1; i < vertexes.length; i++) {
-				if (vertexes[start][i] == 0) {
-					continue;
-				}
 
-				if (!visited[i]) {
-					q.offer(i);
-					visited[i] = true;
+			PriorityQueue<Integer> temp = new PriorityQueue<>();
+			for (int i = 0; i < vertexes[start].size(); i++) {
+				int index = vertexes[start].get(i);
+
+				if (!visited[index]) {
+					temp.offer(index);
+					visited[index] = true;
 				}
+			}
+			for (int min : temp) {
+				q.offer(min);
 			}
 		}
 	}
